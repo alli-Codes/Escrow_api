49ad4c8d490f3247e64cd2d576d4318addc4a5dd
alli-Codes <allisonevans081@gmail.com>
2025-12-15 17:30:15 +0100

    feat: make a function that gets users from the db

--- src/controllers/getUserEscrows.ts
+++ src/controllers/getUserEscrows.ts
@@ -1,11 +1,13 @@
-import Escrow from "../models/Escrows"
+import Escrow from "../models/Escrow"
 
-const getUserEscrows = function(id){
+const getUserEscrows = function(id: string){
 	const escrows = Escrow.findAll({
 		where: {
-			buyerId: id
+			buyerId: id,
 		}
 	});
 
 	return escrows;
 }
+
+export default getUserEscrows;
