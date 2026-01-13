40d470219d3317140ac9365a472e18bb6ec4258c
alli-Codes <allisonevans081@gmail.com>
2026-01-13 23:22:07 +0100

    fix: clean code

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,6 @@ server.use(express.urlencoded({ extended: true }));
 server.use(users);
 server.use("/api/v1/escrow", routes.escrow);
 server.use("/api/v1/users", routes.users);
-// server.get("/api/v1/users", (req, res) => res.send(users));
 
 server.listen(port, () =>
   console.log(colors.green(`Server is running on port ${port}`)),
